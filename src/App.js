af36382642bffc315c1cc63ef73b2d22fb509ecd
Tushar Upadhyay <tusharupadhyay691@gmail.com>
2023-02-03 18:12:14 +0530

    f

--- src/App.js
+++ src/App.js
@@ -9,49 +9,52 @@ import Quiz from "./Pages/Quiz/Quiz";
 import Result from "./Pages/Result/Result";
 
 function App() {
-  const [questions, setQuestions] = useState();
-  const [name, setName] = useState();
-  const [score, setScore] = useState(0);
+    const [questions, setQuestions] = useState();
+    const [name, setName] = useState();
+    const [score, setScore] = useState(0);
 
-  const fetchQuestions = async (category = "", difficulty = "") => {
-    const { data } = await axios.get(
-      `https://opentdb.com/api.php?amount=10${
-        category && `&category=${category}`
-      }${difficulty && `&difficulty=${difficulty}`}&type=multiple`
+    const fetchQuestions = async (category = "", difficulty = "") => {
+        const { data } = await axios.get(
+            `https://opentdb.com/api.php?amount=10${
+                category && `&category=${category}`
+            }${difficulty && `&difficulty=${difficulty}`}&type=multiple`
+        );
+
+        setQuestions(data.results);
+    };
+
+    return (
+        <BrowserRouter>
+            <div
+                className="app"
+                style={{ backgroundImage: 'url("/ques1.png")' }}
+            >
+                <Header />
+                <Route>
+                    <Route path="/" exact>
+                        <Home
+                            name={name}
+                            setName={setName}
+                            fetchQuestions={fetchQuestions}
+                        />
+                    </Route>
+                    <Route path="/quiz">
+                        <Quiz
+                            name={name}
+                            questions={questions}
+                            score={score}
+                            setScore={setScore}
+                            setQuestions={setQuestions}
+                        />
+                    </Route>
+                    <Route path="/result">
+                        <Result name={name} score={score} />
+                    </Route>
+                </Route>
+            </div>
+            <Footer />
+        </BrowserRouter>
     );
-
-    setQuestions(data.results);
-  };
-
-  return (
-    <BrowserRouter>
-      <div className="app" style={{ backgroundImage: 'url("/ques1.png")' }}>
-        <Header />
-        <Switch>
-          <Route path="/" exact>
-            <Home
-              name={name}
-              setName={setName}
-              fetchQuestions={fetchQuestions}
-            />
-          </Route>
-          <Route path="/quiz">
-            <Quiz
-              name={name}
-              questions={questions}
-              score={score}
-              setScore={setScore}
-              setQuestions={setQuestions}
-            />
-          </Route>
-          <Route path="/result">
-            <Result name={name} score={score} />
-          </Route>
-        </Switch>
-      </div>
-      <Footer />
-    </BrowserRouter>
-  );
 }
 
 export default App;
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,20 @@
+const Footer = () => {
+    return (
+        <div
+            style={{
+                textAlign: "center",
+                marginBottom: 10,
+            }}
+        >
+            Made with ♥ by{" "}
+            <a
+                href="https://portfolio-five-puce-56.vercel.app/"
+                style={{ cursor: "pointer" }}
+            >
+                Tushar Upadhyay
+            </a>
+        </div>
+    );
+};
+
+export default Footer;
